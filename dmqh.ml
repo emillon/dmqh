4af3d1be22b8f3dff609ea5cf510a05b58908779
Etienne Millon <me@emillon.org>
2014-03-28 12:22:23 +0100

    filling up

--- dmqh.ml
+++ dmqh.ml
@@ -1,18 +1,57 @@
+let border = 10
+let cell_w = 80
+let cell_h = 80
+let inter_cell = 10
+let win_w = 2 * border + 4 * cell_w + 3 * inter_cell
+let win_h = 2 * border + 4 * cell_h + 3 * inter_cell
+let bgcolor = 0xff0000l
+let emptycolor = 0x00ff00l
+
 type tile = T2 | T4 | T8 | T16 | T32 | T64 | T128 | T256 | T512 | T1024 | T2048
+
+let all_tiles =
+  [T2 ; T4 ; T8 ; T16 ; T32 ; T64 ; T128 ; T256 ; T512 ; T1024 ; T2048]
 
 type board = tile option array array
 
 let empty_board () =
   Array.make_matrix 4 4 None
 
-let add_random b =
-  assert false
+let random_list l =
+  let len = List.length l in
+  List.nth l (Random.int len)
 
-let clear b =
-  assert false
+let random_tile () =
+  random_list all_tiles
+
+let random_avail b =
+  let (>>=) l f = List.concat (List.map f l) in
+  let all_idx = [0;1;2;3] in
+  let all_idx2 =
+    all_idx >>= fun i ->
+    all_idx >>= fun j ->
+    [(i, j)]
+  in
+  let avail = List.filter (fun (i, j) -> b.(i).(j) = None) all_idx2 in
+  random_list avail
+
+let add_random b =
+  let (i, j) = random_avail b in
+  b.(i).(j) <- Some (random_tile ())
+
+let clear win =
+  Sdlvideo.fill_rect win bgcolor
+
+let rect_at i j =
+  let x = border + i * (cell_w + inter_cell) in
+  let y = border + j * (cell_h + inter_cell) in
+  let w = cell_w in
+  let h = cell_h in
+  Sdlvideo.rect ~x ~y ~w ~h
 
 let draw_empty win i j =
-  assert false
+  let rect = rect_at i j in
+  Sdlvideo.fill_rect ~rect win emptycolor
 
 let draw_tile win i j t =
   assert false
@@ -36,13 +75,6 @@ let wait_event win =
 
 let move d =
   assert false
-
-let border = 10
-let cell_w = 80
-let cell_h = 80
-let inter_cell = 10
-let win_w = 2 * border + 4 * cell_w + 3 * inter_cell
-let win_h = 2 * border + 4 * cell_h + 3 * inter_cell
 
 let main () =
   Sdl.init ~auto_clean:true [`VIDEO];

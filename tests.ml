a1a4de3ef9e1cfdf544b93f2d28ad02a25daa1cf
Etienne Millon <me@emillon.org>
2014-03-28 18:15:07 +0100

    alcotest

--- tests.ml
+++ tests.ml
@@ -8,18 +8,20 @@ let run_tests () =
     let printer l =
       p_list pr_tileo l
     in
-    OUnit.assert_equal ~printer exp (move_row input)
+    (printer input, `Quick, fun () ->
+      OUnit.assert_equal ~printer exp (move_row input)
+    )
   in
-  List.iter tc
-    [ [None;None;None;None], [None;None;None;None]
-    ; [None;Some T2;None;None], [Some T2;None;None;None]
-    ; [Some T2;Some T2;None;None], [Some T4;None;None;None]
-    ; [None;Some T2;Some T2;None], [Some T4;None;None;None]
-    ; [Some T16;Some T2;Some T2;None], [Some T16;Some T4;None;None]
-    ; [Some T2;Some T2;Some T4;Some T4], [Some T4;Some T8;None;None]
-    ; [Some T2;Some T2;Some T2;None], [Some T4;Some T2;None;None]
-    ; [Some T16;None;Some T2;Some T2], [Some T16;Some T4;None;None]
-    ];
-  print_endline "OK"
+  Alcotest.run "DMQH" [("move", List.map tc
+      [ [None;None;None;None], [None;None;None;None]
+      ; [None;Some T2;None;None], [Some T2;None;None;None]
+      ; [Some T2;Some T2;None;None], [Some T4;None;None;None]
+      ; [None;Some T2;Some T2;None], [Some T4;None;None;None]
+      ; [Some T16;Some T2;Some T2;None], [Some T16;Some T4;None;None]
+      ; [Some T2;Some T2;Some T4;Some T4], [Some T4;Some T8;None;None]
+      ; [Some T2;Some T2;Some T2;None], [Some T4;Some T2;None;None]
+      ; [Some T16;None;Some T2;Some T2], [Some T16;Some T4;None;None]
+      ]
+  )]
 
 let _ = run_tests ()
